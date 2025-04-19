8d3e6f207d0946c717ca59a5ba3b8af4de2a000a
bruce-mig <brucetmig@icloud.com>
2025-04-19 17:28:13 +0200

    merge sort cpp

--- merge-sort.cpp
+++ merge-sort.cpp
@@ -12,77 +12,81 @@ using namespace std;
 // First subarray is arr[left..mid]
 // Second subarray is arr[mid+1..right]
 void merge(vector<int> &arr, int left, int mid, int right) {
-  int n1 = mid - left + 1;
-  int n2 = right - mid;
+   int n1 = mid - left + 1;
+   int n2 = right - mid;
 
-  // Create temp vectors
-  vector<int> L(n1), R(n2);
+   // Create temp vectors
+   vector<int> L(n1), R(n2);
 
-  // Copy data to temp vectors L[] and R[]
-  for (int i = 0; i < n1; i++) L[i] = arr[left + i];
-  for (int j = 0; j < n2; j++) R[j] = arr[mid + 1 + j];
+   // Copy data to temp vectors L[] and R[]
+   for (int i = 0; i < n1; i++)
+      L[i] = arr[left + i];
+   for (int j = 0; j < n2; j++)
+      R[j] = arr[mid + 1 + j];
 
-  int i = 0, j = 0;
-  int k = left;
+   int i = 0, j = 0;
+   int k = left;
 
-  // Merge the temp vectors back
-  // into arr[left..right]
-  while (i < n1 && j < n2) {
-    if (L[i] <= R[j]) {
+   // Merge the temp vectors back
+   // into arr[left..right]
+   while (i < n1 && j < n2) {
+      if (L[i] <= R[j]) {
+         arr[k] = L[i];
+         i++;
+      } else {
+         arr[k] = R[j];
+         j++;
+      }
+      k++;
+   }
+
+   // Copy the remaining elements of L[],
+   // if there are any
+   while (i < n1) {
       arr[k] = L[i];
       i++;
-    } else {
+      k++;
+   }
+
+   // Copy the remaining elements of R[],
+   // if there are any
+   while (j < n2) {
       arr[k] = R[j];
       j++;
-    }
-    k++;
-  }
-
-  // Copy the remaining elements of L[],
-  // if there are any
-  while (i < n1) {
-    arr[k] = L[i];
-    i++;
-    k++;
-  }
-
-  // Copy the remaining elements of R[],
-  // if there are any
-  while (j < n2) {
-    arr[k] = R[j];
-    j++;
-    k++;
-  }
+      k++;
+   }
 }
 
 // begin is for left index and end is right index
 // of the sub-array of arr to be sorted
 void mergeSort(vector<int> &arr, int left, int right) {
-  if (left >= right) return;
+   if (left >= right)
+      return;
 
-  int mid = left + (right - left) / 2;
-  mergeSort(arr, left, mid);
-  mergeSort(arr, mid + 1, right);
-  merge(arr, left, mid, right);
+   int mid = left + (right - left) / 2;
+   mergeSort(arr, left, mid);
+   mergeSort(arr, mid + 1, right);
+   merge(arr, left, mid, right);
 }
 
 // Function to print a vector
 void printVector(vector<int> &arr) {
-  for (int i = 0; i < arr.size(); i++) cout << arr[i] << " ";
-  cout << endl;
+   for (int i = 0; i < arr.size(); i++)
+      cout << arr[i] << " ";
+   cout << endl;
 }
 
 // Driver code
 int main() {
-  vector<int> arr = {2, 4, 1, 6, 8, 5, 3, 7};
-  int n = arr.size();
+   vector<int> arr = {2, 4, 1, 6, 8, 5, 3, 7};
+   int n = arr.size();
 
-  cout << "Given vector is \n";
-  printVector(arr);
+   cout << "Given vector is \n";
+   printVector(arr);
 
-  mergeSort(arr, 0, n - 1);
+   mergeSort(arr, 0, n - 1);
 
-  cout << "\nSorted vector is \n";
-  printVector(arr);
-  return 0;
+   cout << "\nSorted vector is \n";
+   printVector(arr);
+   return 0;
 }
